66aa7a2bdf1ab38096f8134cef0859d5d6d86247
wownsl456 <wownsl456@naver.com>
2021-04-01 22:48:47 +0900

    React

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Subject extends Component {
+    render() {
+        return (
+            <header>
+                <h1>{this.props.title}</h1>
+                    {this.props.sub}
+            </header>
+        );
+    }
+}
+ 
+class TOC extends Component {
+    render() {   
+        return ( 
+            <nav>
+                <ul>
+                    <li><a href="1.html">HTML</a></li>
+                    <li><a href="2.html">CSS</a></li>
+                    <li><a href="3.html">Javascript</a></li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+class Content extends Component {
+    render() {
+        return (
+            <article>
+                <h2>{this.props.title}</h2>
+                    {this.props.desc}
+            </article>
+        );
+    }
+}
+
 class App extends Component {
     render() {
         return (
             <div className="App">
-                Hello, React!!
+                <Subject title="WEB" sub="world wide web!"></Subject>
+                <TOC></TOC>
+                <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
             </div>
         );
     }
