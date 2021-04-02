d1adaba2849082ce520ea53fbad43fbcd2ff4eae
wownsl456 <wownsl456@naver.com>
2021-04-02 17:08:01 +0900

    React

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,52 +1,54 @@
 import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import './App.css';
 
-class Subject extends Component {
-    render() {
-        return (
-            <header>
-                <h1>{this.props.title}</h1>
-                    {this.props.sub}
-            </header>
-        );
-    }
-}
- 
-class TOC extends Component {
-    render() {   
-        return ( 
-            <nav>
-                <ul>
-                    <li><a href="1.html">HTML</a></li>
-                    <li><a href="2.html">CSS</a></li>
-                    <li><a href="3.html">Javascript</a></li>
-                </ul>
-            </nav>
-        );
-    }
-}
-
-class Content extends Component {
-    render() {
-        return (
-            <article>
-                <h2>{this.props.title}</h2>
-                    {this.props.desc}
-            </article>
-        );
-    }
-}
-
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            mode:'welcome',
+            subject:{title:'WEB', sub:'World Wide Web!'},
+            welcome:{title:'Welcome', desc:'Hello, React!!'},
+            contents:[
+                {id:1, title:'HTML', desc:'HTML is for information'},
+                {id:2, title:'CSS', desc:'CSS is for design'},
+                {id:3, title:'JavaScript', desc:'JavaSCript is for interactive'},
+            ]
+        }
+    }
     render() {
+        var _title, _desc = null;
+        if(this.state.mode === 'welcome'){
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if(this.state.mode === 'read'){
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
         return (
             <div className="App">
-                <Subject title="WEB" sub="world wide web!"></Subject>
-                <TOC></TOC>
-                <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+                {/* <Subject  
+                title={this.state.subject.title} 
+                sub={this.state.subject.sub}>  
+                </Subject> */}
+                <header>
+                    <h1><a href="/" onClick={function(e){
+                        console.log(e);
+                        e.preventDefault();
+                        // this.state.mode= 'welcome';
+                        this.setState({
+                            mode:'welcome'
+                        });
+                    }.bind(this)}>{this.state.subject.title}</a></h1>
+                    {this.state.subject.sub}
+                </header>
+                <TOC data={this.state.contents}></TOC>
+                <Content title={_title} desc={_desc}></Content>
             </div>
         );
     }
 }
-
+        
 export default App;
